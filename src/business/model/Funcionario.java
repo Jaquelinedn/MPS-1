e8d8403ab0d88f5774a856fadca45b1f8e621959
James <James@192.168.25.3>
2017-05-25 19:19:30 -0300

    Adicionado as entidades professor e funcionário

---
+++ src/business/model/Funcionario.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author James
+ */
+public class Funcionario {
+    private String nome;
+    private String siape;
+    private String funcao;
+    private Date data_nascimento;
+
+    public Funcionario(String nome, String siape, String funcao, Date data_nascimento) {
+        this.nome = nome;
+        this.siape = siape;
+        this.funcao = funcao;
+        this.data_nascimento = data_nascimento;
+    }
+    
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSiape() {
+        return siape;
+    }
+
+    public void setSiape(String siape) {
+        this.siape = siape;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+    
+    
+}
---
+++ src/business/model/Professor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author James
+ */
+public class Professor extends Funcionario {
+    private String titulo;
+    private String cargo;
+
+    public Professor(String titulo, String cargo, String nome, String siape, String funcao, Date data_nascimento) {
+        super(nome, siape, funcao, data_nascimento);
+        this.titulo = titulo;
+        this.cargo = cargo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+    
+}
